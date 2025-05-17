dbc3741b55a6ffd5055afaf0708f10fea12d43ca
Ciro Mc <156836104+Ciricundio@users.noreply.github.com>
2025-05-17 13:06:14 -0500

    actualizar archivo script y sesion, y cambiar nombre a index

--- sesion.js
+++ sesion.js
@@ -3,7 +3,7 @@ function iniciarSesion() {
     const password = document.getElementById("password").value;
 
     if (username === "Ciro Muñoz" && password === "1082") {
-        window.location.href="index.html";
+        window.location.href="suCuenta.html";
     } else {
         alert("Nombre de usuario o contraseña incorrectos");
     }
